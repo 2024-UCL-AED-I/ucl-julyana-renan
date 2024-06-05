38e6a1ba7f30aba182c4d363435bf6cb3a638727
JuCordeir <julyanacordeiro@ucl.br>
2024-06-04 23:50:29 -0300

    Inclusão Classe Emprestimo

---
+++ classeEmprestimo.cs
@@ -0,0 +1,22 @@
+using DateTime;
+
+class Emprestimo{
+
+    private Livro livro;
+    private Pessoa cliente;
+    private 
+
+    public aluguelLivro(Livro livro, Pessoa cliente){
+
+        this.livro = livro;
+        this.cliente = cliente;
+
+
+    }
+
+    public devolucaoLivro(Livro livro){
+
+        
+    }
+
+}
